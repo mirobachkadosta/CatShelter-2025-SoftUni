5a7b3c2748f5ae4cd3dfd2091d27db2a1777232e
Miroslav Dimitrov <mdimitrov963@gmail.com>
2025-09-13 14:29:35 +0300

    Add basic http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.write('Hello world !');
+
+  res.end();
+})
+
+server.listen(4000);
+
+console.log('Server is listening on http://localhost:4000...');
